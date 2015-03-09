0050cb594965910f97bbcc501997d0a8f1d6b562
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-03-09 14:24:21 +0100

    Remove useless code in zhelpers.h

--- zhelpers.h
+++ zhelpers.h
@@ -13,33 +13,7 @@
 #endif
 
 #include <zmq.h>
-
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
 #include <string.h>
-#include <time.h>
-#include <sys/time.h>
-
-#include <assert.h>
-#include <signal.h>
-
-/* Version checking, and patch up missing constants to match 2.1 */
-#if ZMQ_VERSION_MAJOR == 2
-#   error "Please upgrade to ZeroMQ/3.2 for these examples"
-#endif
-
-/* On some version of Windows, POSIX subsystem is not installed by default.
- * So define srandom and random ourself.
- */
-#if (defined (WIN32))
-#   define srandom srand
-#   define random rand
-#endif
-
-/* Provide random number from 0..(num-1) */
-#define randof(num)  (int) ((float) (num) * random () / (RAND_MAX + 1.0))
 
 /* Receive 0MQ string from socket and convert into C string
  * Caller must free returned string. Returns NULL if the context
