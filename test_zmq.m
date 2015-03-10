77ce3c381e6c073515a8d659033e5fcb10b0a56b
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-03-10 10:50:37 +0100

    Don't load DLL with LoadLibrary()
    LoadLibrary() (or an equivalent) is already done by Matlab to load the mex
    file (like a plugin). So doing it in the mex file to load other shared
    libraries is useless. The DLL is loaded automatically when loading the mex
    file.

--- test_zmq.m
+++ test_zmq.m
@@ -1,8 +1,7 @@
 clear all;
 
 %zmq_subscriber('init', 'tcp://localhost:5001', 'Pupil');
-zmq_subscriber('init', 'tcp://192.168.1.1:5000', '', ...
-    'C:\Users\sewilmet\Documents\MATLAB\zeromq\libzmq-v100-mt-4_0_4.dll');
+zmq_subscriber('init', 'tcp://192.168.1.1:5000', '');
 
 for i = 1:5
     zmq_subscriber('receive_next_message')
