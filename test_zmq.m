ba976d5695bbd80c0cce2be51c4cf67d62a960dc
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-03-10 10:41:40 +0100

    Load DLL at run-time
    It works! But it's probably because we must load the v100 DLL, not the
    v120 one.

--- test_zmq.m
+++ test_zmq.m
@@ -1,7 +1,8 @@
 clear all;
 
 %zmq_subscriber('init', 'tcp://localhost:5001', 'Pupil');
-zmq_subscriber('init', 'tcp://192.168.1.1:5000', '');
+zmq_subscriber('init', 'tcp://192.168.1.1:5000', '', ...
+    'C:\Users\sewilmet\Documents\MATLAB\zeromq\libzmq-v100-mt-4_0_4.dll');
 
 for i = 1:5
     zmq_subscriber('receive_next_message')
--- zmq_subscriber.c
+++ zmq_subscriber.c
@@ -1,9 +1,33 @@
 /* Subscriber - listen everything. */
 
+#include <zmq.h>
+#include <windows.h>
 #include <string.h>
 #include <assert.h>
-#include "zhelpers.h"
 #include "mex.h"
+
+/* Support multiple initializations, because if the Matlab program crashes, the process
+ * isn't killed.
+ */
+static void *context = NULL;
+static void *subscriber = NULL;
+
+/* Boilerplate for loading DLL functions. */
+typedef void * (* ZmqCtxNew) (void);
+typedef int (* ZmqClose) (void *s);
+typedef void * (* ZmqSocket) (void *, int type);
+typedef int (* ZmqConnect) (void *s, const char *addr);
+typedef int (* ZmqSetsockopt) (void *s, int option, const void *optval, size_t optvallen);
+typedef int (* ZmqRecv) (void *s, void *buf, size_t len, int flags);
+typedef int (* ZmqSend) (void *s, const void *buf, size_t len, int flags);
+
+static ZmqCtxNew zmq_ctx_new_func = NULL;
+static ZmqClose zmq_close_func = NULL;
+static ZmqSocket zmq_socket_func = NULL;
+static ZmqConnect zmq_connect_func = NULL;
+static ZmqSetsockopt zmq_setsockopt_func = NULL;
+static ZmqRecv zmq_recv_func = NULL;
+static ZmqSend zmq_send_func = NULL;
 
 #if (defined (WIN32))
 /* The function is available on GNU/Linux, but not on Windows.
@@ -30,11 +54,82 @@ strsep (char **sp, char *sep)
 }
 #endif
 
-/* Support multiple initializations, if the Matlab program crashes, the process
- * isn't killed.
+static void
+load_zmq_functions (const char *path_to_dll)
+{
+	static HINSTANCE handle = NULL;
+
+	if (handle != NULL)
+	{
+		mexPrintf ("zmq_subscriber info: DLL file already loaded.\n");
+		return;
+	}
+
+	handle = LoadLibrary (path_to_dll);
+
+	if (handle == NULL)
+	{
+		mexErrMsgTxt ("zmq_subscriber error when loading the DLL.");
+		return;
+	}
+	
+	zmq_ctx_new_func = (ZmqCtxNew) GetProcAddress (handle, "zmq_ctx_new");
+	zmq_close_func = (ZmqClose) GetProcAddress (handle, "zmq_close");
+	zmq_socket_func = (ZmqSocket) GetProcAddress (handle, "zmq_socket");
+	zmq_connect_func = (ZmqConnect) GetProcAddress (handle, "zmq_connect");
+	zmq_setsockopt_func = (ZmqSetsockopt) GetProcAddress (handle, "zmq_setsockopt");
+	zmq_recv_func = (ZmqRecv) GetProcAddress (handle, "zmq_recv");
+	zmq_send_func = (ZmqSend) GetProcAddress (handle, "zmq_send");
+
+	if (zmq_ctx_new_func == NULL ||
+	    zmq_close_func == NULL ||
+	    zmq_socket_func == NULL ||
+	    zmq_connect_func == NULL ||
+	    zmq_setsockopt_func == NULL ||
+	    zmq_recv_func == NULL ||
+	    zmq_send_func == NULL)
+	{
+		FreeLibrary (handle);
+		handle = NULL;
+		mexErrMsgTxt ("zmq_subscriber error: some function(s) in the DLL file can not be loaded.");
+		return;
+	}
+
+	mexPrintf ("zmq_subscriber info: DLL file initialization OK.\n");
+}
+
+/* Receive 0MQ string from socket and convert into C string
+ * Caller must free returned string. Returns NULL if the context
+ * is being terminated.
+ * Function taken from zhelpers.
  */
-static void *context = NULL;
-static void *subscriber = NULL;
+static char *
+s_recv (void *socket) {
+	char buffer [256];
+	int size = zmq_recv_func (socket, buffer, 255, 0);
+	
+	if (size == -1)
+	{
+		return NULL;
+	}
+	
+	if (size > 255)
+	{
+		size = 255;
+	}
+	
+	buffer [size] = 0;
+	return strdup (buffer);
+}
+
+/* Convert C string to 0MQ string and send to socket.
+ * Function taken from zhelpers.
+ */
+static int
+s_send (void *socket, char *string) {
+	int size = zmq_send_func (socket, string, strlen (string), 0);
+	return size;
+}
 
 static void
 init_zmq (const char *end_point,
@@ -44,23 +139,23 @@ init_zmq (const char *end_point,
 
 	if (context == NULL)
 	{
-		context = zmq_ctx_new ();
+		context = zmq_ctx_new_func ();
 	}
 
 	if (subscriber != NULL)
 	{
-		zmq_close (subscriber);
+		zmq_close_func (subscriber);
 	}
 
-	subscriber = zmq_socket (context, ZMQ_SUB);
+	subscriber = zmq_socket_func (context, ZMQ_SUB);
 
-	ok = zmq_connect (subscriber, end_point);
+	ok = zmq_connect_func (subscriber, end_point);
 	if (ok != 0)
 	{
 		mexErrMsgTxt ("zmq_subscriber error: impossible to connect to the end point.");
 	}
 
-	ok = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, filter, strlen (filter));
+	ok = zmq_setsockopt_func (subscriber, ZMQ_SUBSCRIBE, filter, strlen (filter));
 	if (ok != 0)
 	{
 		mexErrMsgTxt ("zmq_subscriber error: impossible to set filter.");
@@ -189,6 +284,7 @@ mexFunction (int n_return_values,
 	{
 		char *end_point;
 		char *filter;
+		char *path_to_dll;
 
 		if (n_return_values > 0)
 		{
@@ -196,18 +292,21 @@ mexFunction (int n_return_values,
 				      "you cannot assign a result with this call.");
 		}
 
-		if (n_args > 3)
+		if (n_args > 4)
 		{
 			mexErrMsgTxt ("zmq_subscriber error: init command: too many arguments.");
 		}
 
 		end_point = mxArrayToString (args[1]);
 		filter = mxArrayToString (args[2]);
+		path_to_dll = mxArrayToString (args[3]);
 
+		load_zmq_functions (path_to_dll);
 		init_zmq (end_point, filter);
 
 		mxFree (end_point);
 		mxFree (filter);
+		mxFree (path_to_dll);
 	}
 	else if (strcmp (command, "receive_next_message") == 0)
 	{
