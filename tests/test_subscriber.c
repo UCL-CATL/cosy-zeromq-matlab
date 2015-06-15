3f9f7aba4628d8d94fbf7f130ddbbc07f7891504
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-06-15 17:13:10 +0200

    Test one subscriber per publisher
    It still works.

--- tests/test_subscriber.c
+++ tests/test_subscriber.c
@@ -4,41 +4,51 @@ int
 main (void)
 {
 	void *context;
-	void *subscriber;
+	void *subscriber_eye;
+	void *subscriber_world;
 	char *filter;
 	int i;
 	int ok;
 
 	context = zmq_ctx_new ();
-	subscriber = zmq_socket (context, ZMQ_SUB);
-	ok = zmq_connect (subscriber, "tcp://localhost:5000");
+	subscriber_eye = zmq_socket (context, ZMQ_SUB);
+	ok = zmq_connect (subscriber_eye, "tcp://localhost:5000");
 	assert (ok == 0);
 
-	ok = zmq_connect (subscriber, "tcp://localhost:5001");
+	subscriber_world = zmq_socket (context, ZMQ_SUB);
+	ok = zmq_connect (subscriber_world, "tcp://localhost:5001");
 	assert (ok == 0);
 
 	filter = "Pupil";
-	ok = zmq_setsockopt (subscriber,
+	ok = zmq_setsockopt (subscriber_eye,
 			     ZMQ_SUBSCRIBE,
 			     filter,
 			     strlen (filter));
 	assert (ok == 0);
 
 	filter = "Gaze";
-	ok = zmq_setsockopt (subscriber,
+	ok = zmq_setsockopt (subscriber_world,
 			     ZMQ_SUBSCRIBE,
 			     filter,
 			     strlen (filter));
 	assert (ok == 0);
 
-	for (i = 0; i < 20; i++)
+	for (i = 0; i < 4; i++)
 	{
-		char *msg = s_recv (subscriber);
-		printf ("%s\n", msg);
+		char *msg = s_recv (subscriber_eye);
+		printf ("Eye:\n%s\n", msg);
 		free (msg);
 	}
 
-	zmq_close (subscriber);
+	for (i = 0; i < 4; i++)
+	{
+		char *msg = s_recv (subscriber_world);
+		printf ("World:\n%s\n", msg);
+		free (msg);
+	}
+
+	zmq_close (subscriber_eye);
+	zmq_close (subscriber_world);
 	zmq_ctx_destroy (context);
 	return 0;
 }
