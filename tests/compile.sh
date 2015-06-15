b28046c45f4a2240d69c8ced1e80605288ced409
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-06-15 14:52:48 +0200

    Move test programs to tests/ directory

--- tests/compile.sh
+++ tests/compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-gcc -o zmq_publisher $(pkg-config --cflags --libs libczmq) zmq_publisher.c zhelpers.h
+gcc -o test_publisher $(pkg-config --cflags --libs libczmq) test_publisher.c zhelpers.h
 gcc -o test_subscriber $(pkg-config --cflags --libs libczmq) test_subscriber.c zhelpers.h
