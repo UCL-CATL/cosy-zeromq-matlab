a78370884810fe234e29bfd4292e91d2efe953dc
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-06-18 10:05:48 +0200

    tests: longer messages
    Messages of length 255, at 100 Hz during two minutes: all the messages
    are kept in the queue.

--- tests/test_publisher.c
+++ tests/test_publisher.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include "zhelpers.h"
 
-#define MAX_MSG_LENGTH 255
+#define MAX_MSG_LENGTH 256
 #define SLEEP_TIME 10 /* in ms */
 
 int
@@ -39,10 +39,17 @@ main (int argc,
 	while (1)
 	{
 		char msg[MAX_MSG_LENGTH];
+		int i;
 
 		usleep (SLEEP_TIME * 1000);
 
 		snprintf (msg, MAX_MSG_LENGTH, "%s\nnum:%d\n", tag, msg_num);
+		for (i = strlen (msg); i < MAX_MSG_LENGTH - 1; i++)
+		{
+			msg[i] = '0';
+		}
+		msg[MAX_MSG_LENGTH - 1] = '\0';
+		assert (strlen (msg) == MAX_MSG_LENGTH - 1);
 
 		s_send (publisher, msg);
 		s_send (publisher, "Other\nblah:1337\n");
