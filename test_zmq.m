db262da8425bffff89b5c375bcfd9692033eb591
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-06-22 17:30:27 +0200

    test_zmq.m: test 100 Hz during 2 minutes
    It works, but the messages are small. I need to test with longer
    messages (of length ~255).

--- test_zmq.m
+++ test_zmq.m
@@ -8,12 +8,19 @@ zmq_subscriber('add_filter', subscriber_eye, 'Pupil');
 subscriber_world = zmq_subscriber('add_subscriber', 'tcp://localhost:5001');
 zmq_subscriber('add_filter', subscriber_world, 'Gaze');
 
-for i = 1:3
-    zmq_subscriber('receive_next_message', subscriber_eye)
+last = 100 * 60 * 2;
+for i = 1:last
+    msg = zmq_subscriber('receive_next_message', subscriber_eye);
+    if i == 1 || i == last
+        msg
+    end
 end
 
-for i = 1:3
-    zmq_subscriber('receive_next_message', subscriber_world)
+for i = 1:last
+    msg = zmq_subscriber('receive_next_message', subscriber_world);
+    if i == 1 || i == last
+        msg
+    end
 end
 
 zmq_subscriber('close');
