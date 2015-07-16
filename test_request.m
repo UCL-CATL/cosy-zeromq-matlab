4df9cedbdfe096d5316a525f9fae0eaf7d0f0c00
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-07-16 17:13:44 +0200

    Test several requesters in Matlab
    It works, when the repliers are correctly launched.

--- test_request.m
+++ test_request.m
@@ -2,8 +2,15 @@ clear all;
 
 zmq_request('init');
 
-requester = zmq_request('add_requester', 'tcp://localhost:5555');
-zmq_request('send_request', requester, 'hello');
-reply = zmq_request('receive_reply', requester, 3000)
+requester1 = zmq_request('add_requester', 'tcp://localhost:5555');
+requester2 = zmq_request('add_requester', 'tcp://localhost:5556');
+
+for i = 1:2
+    zmq_request('send_request', requester1, 'bing');
+    reply1 = zmq_request('receive_reply', requester1, 3000)
+
+    zmq_request('send_request', requester2, 'bong');
+    reply2 = zmq_request('receive_reply', requester2, 3000)
+end
 
 zmq_request('close');
