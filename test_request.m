26b18ab9ec4b1c11c6b0bc9af8e92cbd65461982
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-07-27 11:27:57 +0200

    test_request.m: loop through the requesters
    Instead of code duplication.

--- test_request.m
+++ test_request.m
@@ -2,15 +2,25 @@ clear all;
 
 zmq_request('init');
 
-requester1 = zmq_request('add_requester', 'tcp://localhost:5555');
-requester2 = zmq_request('add_requester', 'tcp://localhost:5556');
+requesters = [];
+requesters(end + 1) = zmq_request('add_requester', 'tcp://localhost:5555');
+requesters(end + 1) = zmq_request('add_requester', 'tcp://localhost:5556');
+
+requesters = int32(requesters);
+
+requests = {'bing', 'bong'};
 
 for i = 1:2
-    zmq_request('send_request', requester1, 'bing');
-    reply1 = zmq_request('receive_reply', requester1, 3000)
+    for requester_num = 1:length(requesters)
+        requester = requesters(requester_num);
+        request_msg = requests{requester_num};
 
-    zmq_request('send_request', requester2, 'bong');
-    reply2 = zmq_request('receive_reply', requester2, 3000)
+        zmq_request('send_request', requester, request_msg);
+        reply = zmq_request('receive_reply', requester, 3000)
+        if isnan(reply)
+            break;
+        end
+    end
 end
 
 zmq_request('close');
