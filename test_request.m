8ae10e79d988d7d92fb4e0609e063dc379e44e87
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2015-07-27 11:36:43 +0200

    test_request.m: add debug messages
    When the replier isn't connected, the send doesn't block (fine), but the
    close blocks.
    
    And there is no way to kill the matlab script.

--- test_request.m
+++ test_request.m
@@ -5,22 +5,38 @@ zmq_request('init');
 requesters = [];
 requesters(end + 1) = zmq_request('add_requester', 'tcp://localhost:5555');
 requesters(end + 1) = zmq_request('add_requester', 'tcp://localhost:5556');
+disp('Requesters connected');
 
 requesters = int32(requesters);
 
 requests = {'bing', 'bong'};
 
+aborted = false;
 for i = 1:2
+    if aborted
+        break;
+    end
+
     for requester_num = 1:length(requesters)
         requester = requesters(requester_num);
         request_msg = requests{requester_num};
 
+        disp('Send request...');
         zmq_request('send_request', requester, request_msg);
+        disp('...done.');
+
+        disp('Receive reply...');
         reply = zmq_request('receive_reply', requester, 3000)
         if isnan(reply)
+            aborted = true;
             break;
         end
     end
 end
 
+disp('Closing...');
+% "force" the display of the message
+pause(2);
+
 zmq_request('close');
+disp('...done.');
