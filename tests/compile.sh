7e4b5e0bbb1a4df3c8c5071f9616f4aa78631dde
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2016-04-19 11:46:34 +0200

    tests: replace compile.sh by a Makefile
    To be more standard.

--- tests/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-gcc -o test_publisher -O2 $(pkg-config --cflags --libs libczmq) test_publisher.c zhelpers.h
-gcc -o test_subscriber -O2 $(pkg-config --cflags --libs libczmq) test_subscriber.c zhelpers.h
-gcc -o test_request -O2 $(pkg-config --cflags --libs libczmq) test_request.c
-gcc -o test_reply -O2 $(pkg-config --cflags --libs libczmq) test_reply.c
